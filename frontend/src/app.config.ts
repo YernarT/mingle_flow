0ec53195c3b4770cc777af0c32a453b8833b3537
YernarT <toktaryernar@gmail.com>
2023-12-18 16:30:42 +0600

    nuxt icon config

---
+++ frontend/src/app.config.ts
@@ -0,0 +1,10 @@
+export default defineAppConfig({
+  nuxtIcon: {
+    size: "24px",
+    class: "itisit-icon",
+    color: "rgb(255, 66, 122)",
+    aliases: {
+      nuxt: "logos:nuxt-icon",
+    },
+  },
+});
