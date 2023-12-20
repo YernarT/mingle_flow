5ae5c7f1f8af5e1b35df622cf86f87d24b1e3ee9
YernarT <toktaryernar@gmail.com>
2023-12-20 17:53:12 +0600

    feat: :sparkles: Design Section

---
+++ frontend/src/components/index_page/DesignSection.vue
@@ -0,0 +1,40 @@
+<template>
+    <section class="design-section">
+        <img class="design" src="~/assets/image/index_page_design.png" alt="Design">
+    </section>
+</template>
+
+<script setup lang="ts">
+// Vue
+import { defineComponent } from 'vue';
+
+defineComponent({ name: 'DesignSection' });
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.design-section {
+    margin-top: 60px;
+    padding: 0 16px;
+    position: relative;
+    z-index: 2;
+
+    .design {
+        display: block;
+        width: 80%;
+        max-width: 1030px;
+        margin: 0 auto;
+        border-radius: var(--border-radius);
+        box-shadow: rgba(255, 66, 122, 0.2) 0px 2px 8px 0px;
+    }
+
+    @media screen and (max-width: 576px) {
+        margin-top: 40px;
+
+        .design {
+            width: 100%;
+        }
+    }
+}
+</style>
