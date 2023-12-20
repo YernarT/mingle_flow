8d52f53857881ac675f3386f633debb88ad7fd71
YernarT <toktaryernar@gmail.com>
2023-12-20 18:07:55 +0600

    update: 增加行高配置项

---
+++ frontend/src/components/index_page/HeaderComp.vue
@@ -0,0 +1,38 @@
+<template>
+    <header class="itisit-container header">
+        <div class="logo">
+            <img src="~/assets/image/logo.png" alt="Mingle Flow">
+            <span>Mingle Flow</span>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+// Vue
+import { defineComponent } from 'vue';
+
+defineComponent({ name: 'HeaderComp' });
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.header {
+    height: 64px;
+    @include flex($alignItems: center);
+
+    .logo {
+        @include flex($alignItems: center, $gap: 8px);
+
+        img {
+            width: 34px;
+            height: 34px;
+            object-fit: cover;
+        }
+
+        span {
+            font-weight: 500;
+        }
+    }
+}
+</style>
