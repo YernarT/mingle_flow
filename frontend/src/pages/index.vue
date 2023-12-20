52209eab92b76136362dd2ecf502d8e7b63edc9f
YernarT <toktaryernar@gmail.com>
2023-12-20 17:53:40 +0600

    feat: :sparkles: Index 页面

---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="index-page">
+    <HeaderComp />
+    <MainSection />
+    <DesignSection />
+    <FooterComp />
+  </main>
+</template>
+
+<script setup lang="ts">
+// Vue
+import { defineComponent } from 'vue';
+
+// Component
+import HeaderComp from '@/components/index_page/HeaderComp.vue';
+import MainSection from '@/components/index_page/MainSection.vue';
+import DesignSection from '@/components/index_page/DesignSection.vue';
+import FooterComp from '@/components/index_page/FooterComp.vue';
+
+defineComponent({ name: 'IndexPage' });
+</script>
