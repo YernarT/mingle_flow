6dbc0d28142bae09dc78edc50c988c63a47f46c8
YernarT <toktaryernar@gmail.com>
2024-01-18 23:51:28 +0600

    init: :rainbow: 常量合集

---
+++ frontend/src/constants/project.ts
@@ -0,0 +1,43 @@
+export const PROJECT = {
+  filter: [
+    {
+      label: "Барлығы",
+      value: "all",
+    },
+    {
+      label: "Жоспарлауда",
+      value: "planing",
+    },
+    {
+      label: "Орындалуда",
+      value: "in-progress",
+    },
+    {
+      label: "Күтуде",
+      value: "paused",
+    },
+    {
+      label: "Аяқталды",
+      value: "finished",
+    },
+  ],
+
+  sort: [
+    {
+      label: "Уақыт б-ша",
+      value: "time",
+    },
+    {
+      label: "Мүше б-ша",
+      value: "contributor",
+    },
+    {
+      label: "Тапсырма б-ша",
+      value: "task",
+    },
+    {
+      label: "Прогресс б-ша",
+      value: "progress",
+    },
+  ],
+};
