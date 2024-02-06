07808c6d8c3928bb9e9a797f0f9df97eef633737
YernarT <toktaryernar@gmail.com>
2024-02-06 19:40:28 +0600

    feat: :sparkles: project card

---
+++ frontend/src/components/business/ProjectCard/Status.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="status"></div>
+</template>
+
+<script setup lang="ts">
+// Vue
+import { defineComponent } from 'vue';
+
+defineComponent({ name: 'ProjectStatus' });
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.status {}
+</style>
---
+++ frontend/src/components/business/ProjectCard/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="project-card">
+        <div class="head">
+            <div class="col">
+                <h3 class="name">Astana Hub</h3>
+                <div class="status"></div>
+            </div>
+            <div class="row">
+                <Icon name="material-symbols:edit" />
+                <Icon name="mdi:dots-vertical" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+// Vue
+import { defineComponent } from 'vue';
+// Component
+
+
+defineComponent({ name: 'ProjectCard' });
+
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.project-card {
+    border-radius: var(--border-radius);
+    background-color: #fff;
+    transition: box-shadow var(--transition);
+
+    .head {
+        @include flex($justifyContent: space-between);
+
+        .row {
+            @include flex($alignItems: center, $gap: 16px);
+        }
+
+        .col {
+            @include flex($direction: column, $gap: 16px);
+        }
+    }
+
+    &:hover {
+        box-shadow: 0 3px 36px 12px rgba(80, 126, 169, 0.12);
+    }
+}
+</style>
