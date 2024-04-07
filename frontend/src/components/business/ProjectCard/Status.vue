642806ced4e0a5bd7ce3dcabf4bd70b3338fcef6
YernarT <toktaryernar@gmail.com>
2024-04-07 15:49:38 +0500

    feat: :sparkles: status

--- frontend/src/components/business/ProjectCard/Status.vue
+++ frontend/src/components/business/ProjectCard/Status.vue
@@ -1,16 +1,61 @@
 <template>
-    <div class="status"></div>
+  <div class="status" :class="[`status--${status}`]">
+    {{ statusLabel }}
+  </div>
 </template>
 
 <script setup lang="ts">
-// Vue
-import { defineComponent } from 'vue';
+// Types
+import type { T_ProjectStatus } from "@/types/project";
 
-defineComponent({ name: 'ProjectStatus' });
+// Vue
+import { defineComponent, computed } from "vue";
+
+defineComponent({ name: "ProjectStatus" });
+
+const props = defineProps<{ status: T_ProjectStatus }>();
+
+const statusLabel = computed(() => {
+  switch (props.status) {
+    case 0:
+      return "Жоспарлауда";
+    case 1:
+      return "Орындалуда";
+    case 2:
+      return "Күтуде";
+    case 3:
+      return "Аяқталған";
+  }
+});
 </script>
 
 <style scoped lang="scss">
 @import "~/assets/style/mixins.scss";
 
-.status {}
+.status {
+  font-size: 16px;
+  color: var(--status-color);
+  @include flex($alignItems: center, $gap: 6px);
+
+  &::before {
+    content: "";
+    width: 6px;
+    height: 6px;
+    border-radius: 50%;
+    background-color: var(--status-color);
+  }
+
+  &--0 {
+    --status-color: var(--c-primary);
+  }
+  &--1 {
+    --status-color: var(--c-success);
+  }
+  &--2 {
+    --status-color: var(--c-warning);
+  }
+  &--3 {
+    --status-color: var(--c-error);
+  }
+}
 </style>
