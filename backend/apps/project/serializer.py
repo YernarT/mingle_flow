298294035d94a2a980ed374b0187f2577f6a3229
YernarT <toktaryernar@gmail.com>
2024-04-08 20:06:05 +0500

    feat: :sparkles: project serializer

--- backend/apps/project/serializer.py
+++ backend/apps/project/serializer.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from project.models import Project, Contributor, ProjectStatuses
 
-from utils.custom_exception import CustomException
+
+class ProjectSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = '__all__'
 
 
-class ProjectSerializer(serializers.ModelField):
-    pass
-
-
-class ContributorSerializer(serializers.ModelField):
-    pass
+class ContributorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contributor
+        fields = '__all__'
