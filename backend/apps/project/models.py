3745193ef9907d2436091338612d358be72dd992
YernarT <toktaryernar@gmail.com>
2024-04-09 23:31:47 +0500

    feat: :sparkles: status, get_progress

--- backend/apps/project/models.py
+++ backend/apps/project/models.py
@@ -1,18 +1,22 @@
 from django.db import models
+from task.models import Task, TaskStatuses
 
 
 class ProjectStatuses(models.IntegerChoices):
-        PLANNING = 0
-        IN_PROGRESS = 1
-        PAUSED = 2
-        FINISHED = 3
+    PLANNING = 0
+    IN_PROGRESS = 1
+    PAUSED = 2
+    FINISHED = 3
+
 
 class Project(models.Model):
     name = models.CharField(max_length=40, verbose_name='Жоба атауы')
     description = models.CharField(
         max_length=500, null=True, blank=True, verbose_name='Жоба сипаттамасы')
-    status = models.IntegerField(choices=ProjectStatuses.choices, default=ProjectStatuses.PLANNING)
-    creator = models.ForeignKey('user.User', on_delete=models.CASCADE, verbose_name='Құрушы')
+    status = models.IntegerField(
+        choices=ProjectStatuses.choices, default=ProjectStatuses.PLANNING)
+    creator = models.ForeignKey(
+        'user.User', on_delete=models.CASCADE, verbose_name='Құрушы')
     create_time = models.DateTimeField(
         auto_now_add=True, verbose_name='Құрылған уақыт')
 
@@ -24,11 +28,47 @@ class Project(models.Model):
     def __str__(self):
         return self.name
 
+    def get_progress(self, task_list=None):
+        '''
+        获取项目的完成度
+        '''
+        if self.status == ProjectStatuses.FINISHED:
+            return 100
+
+        if task_list == None:
+            task_list = Task.objects.filter(project=self)
+        
+        task_count = task_list.count()
+        
+        if task_count == 0:
+            return 0
+
+        progress = 0
+        task_weights = 100 / task_count
+
+        for task in task_list:
+            if task.status == TaskStatuses.BACKLOG:
+                progress += 1
+            elif task.status == TaskStatuses.TEST:
+                progress += task_weights * 0.75
+            elif task.status == TaskStatuses.DEVELOP:
+                progress += task_weights * 0.5
+            elif task.status == TaskStatuses.ANALYZE:
+                progress += task_weights * 0.25
+            else:
+                progress += 0
+        
+        return progress
+
+
 
 class Contributor(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, verbose_name='Жоба')
-    user = models.ForeignKey('user.User', on_delete=models.CASCADE, verbose_name='Мүше')
-    can_modify_task = models.BooleanField(default=False, verbose_name='Тапсырма құруға/өзгертуге болады')
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE, verbose_name='Жоба')
+    user = models.ForeignKey(
+        'user.User', on_delete=models.CASCADE, verbose_name='Мүше')
+    can_modify_task = models.BooleanField(
+        default=False, verbose_name='Тапсырма құруға/өзгертуге болады')
     join_time = models.DateTimeField(
         auto_now_add=True, verbose_name='Қосылған уақыт')
 
