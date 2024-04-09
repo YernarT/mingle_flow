e9bb515f8f1bd6aeaf50951774126018802836fc
YernarT <toktaryernar@gmail.com>
2024-04-09 23:32:02 +0500

    update: :hammer: to_representation

--- backend/apps/project/serializer.py
+++ backend/apps/project/serializer.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+
+from user.serializer import UserSerializer
 from project.models import Project, Contributor, ProjectStatuses
+from task.models import Task
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -8,8 +11,23 @@ class ProjectSerializer(serializers.ModelSerializer):
         model = Project
         fields = '__all__'
 
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+
+        contributor_list = Contributor.objects.filter(project=instance)
+        contributor_count = contributor_list.count()
+        data['contributor_count'] = contributor_count
+        data['contributor_list'] = ContributorSerializer(
+            instance=contributor_list[:7], many=True).data
+        data['task_count'] = Task.objects.filter(project=instance).count()
+        data['progress'] = instance.get_progress()
+
+        return data
+
 
 class ContributorSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Contributor
         fields = '__all__'
