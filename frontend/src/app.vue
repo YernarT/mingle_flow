48d18876b36cd8baa6506cda69c07acbf0e4c688
YernarT <toktaryernar@gmail.com>
2024-04-29 11:46:05 +0500

    style: :hammer: format document

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,46 +1,53 @@
 <template>
   <Html lang="kk">
+    <Head>
+      <Meta charset="UTF-8" />
+      <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Meta name="theme-color" content="rgb(255, 66, 122)" />
+      <meta name="color-scheme" content="light" />
 
-  <Head>
-    <Meta charset="UTF-8" />
-    <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <Meta name="theme-color" content="rgb(255, 66, 122)" />
-    <meta name="color-scheme" content="light" />
+      <Meta httpEquiv="x-ua-compatible" content="IE=edge" />
+      <Meta httpEquiv="x-ua-compatible" content="IE=7" />
 
-    <Meta httpEquiv="x-ua-compatible" content="IE=edge" />
-    <Meta httpEquiv="x-ua-compatible" content="IE=7" />
+      <Meta
+        name="description"
+        content="Тапсырмаларды, прогресті ұйымдастыруға арналған жобаларды тиімді басқару жүйесі. Жұмыс процестерін оңтайландырып, ынтымақтастықты жақсартып және өнімділікті арттырады."
+      />
+      <Meta
+        name="keywords"
+        content="жобаны басқару, тапсырмаларды ұйымдастыру, топтық ынтымақтастық, жұмыс процесін оңтайландыру, өнімділік"
+      />
+      <Meta name="author" content="https://github.com/YernarT" />
 
-    <Meta name="description"
-      content="Тапсырмаларды, прогресті ұйымдастыруға арналған жобаларды тиімді басқару жүйесі. Жұмыс процестерін оңтайландырып, ынтымақтастықты жақсартып және өнімділікті арттырады." />
-    <Meta name="keywords"
-      content="жобаны басқару, тапсырмаларды ұйымдастыру, топтық ынтымақтастық, жұмыс процесін оңтайландыру, өнімділік" />
-    <Meta name="author" content="https://github.com/YernarT" />
+      <Meta property="og:title" content="Mingle Flow" />
+      <Meta
+        property="og:description"
+        content="Тапсырмаларды, прогресті ұйымдастыруға арналған жобаларды тиімді басқару жүйесі. Жұмыс процестерін оңтайландырып, ынтымақтастықты жақсартып және өнімділікті арттырады."
+      />
+      <Meta property="og:image" content="/favicon.ico" />
+      <Meta property="og:site_name" content="Mingle Flow" />
+      <Meta property="og:type" content="website" />
+      <Meta property="og:locale" content="kk_KZ" />
 
-    <Meta property="og:title" content="Mingle Flow" />
-    <Meta property="og:description"
-      content="Тапсырмаларды, прогресті ұйымдастыруға арналған жобаларды тиімді басқару жүйесі. Жұмыс процестерін оңтайландырып, ынтымақтастықты жақсартып және өнімділікті арттырады." />
-    <Meta property="og:image" content="/favicon.ico" />
-    <Meta property="og:site_name" content="Mingle Flow" />
-    <Meta property="og:type" content="website" />
-    <Meta property="og:locale" content="kk_KZ" />
+      <Link rel="icon" href="/favicon.ico" />
 
-    <Link rel="icon" href="/favicon.ico" />
+      <Link rel="preconnect" href="https://fonts.googleapis.com" />
+      <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+      <Link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap"
+        rel="stylesheet"
+      />
 
-    <Link rel="preconnect" href="https://fonts.googleapis.com" />
-    <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
-    <Link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet" />
+      <Title>Mingle Flow</Title>
+    </Head>
 
-    <Title>Mingle Flow</Title>
-  </Head>
-
-  <Body>
-    <NuxtLayout>
-      <a-config-provider :locale="kk_KZ" :theme="ANTD_THEME">
-        <NuxtPage />
-      </a-config-provider>
-    </NuxtLayout>
-  </Body>
-
+    <Body>
+      <NuxtLayout>
+        <a-config-provider :locale="kk_KZ" :theme="ANTD_THEME">
+          <NuxtPage />
+        </a-config-provider>
+      </NuxtLayout>
+    </Body>
   </Html>
 </template>
 
@@ -48,16 +55,15 @@
 // Vue
 import { onBeforeMount } from "vue";
 // Store
-import { useUser } from "@/stores/user";
+import { useUserStore } from "@/stores/user";
 // Antd
 import kk_KZ from "ant-design-vue/locale/kk_KZ";
-import { ANTD_THEME } from '@/constants/antd';
-
+import { ANTD_THEME } from "@/constants/antd";
 
 // 初始化 Store
 onBeforeMount(() => {
-  const user = useUser();
-  user.initUserFromLocal();
+  const userStore = useUserStore();
+  userStore.initUserFromLocal();
 });
 </script>
 
--- frontend/src/components/profile_page/ProfileSettings.vue
+++ frontend/src/components/profile_page/ProfileSettings.vue
@@ -1,144 +1,204 @@
 <template>
-    <div class="profile-settings">
-        <div class="block">
-            <p class="head">Негізгі ақпарат</p>
+  <div class="profile-settings">
+    <div class="block">
+      <p class="head">Негізгі ақпарат</p>
 
-            <a-form :model="basicInfoForm" layout="vertical" autocomplete="off" @finish="handleChangeInfo">
-                <a-form-item label="Аты-жөн" name="fullname"
-                    :rules="[{ required: true, message: 'Аты-жөн нөмерді енгізіңіз!' }]">
-                    <a-input v-model:value="basicInfoForm.fullname" maxLength="30" />
-                </a-form-item>
+      <a-form
+        :model="basicInfoForm"
+        layout="vertical"
+        autocomplete="off"
+        @finish="handleChangeInfo"
+      >
+        <a-form-item
+          label="Аты-жөн"
+          name="fullname"
+          :rules="[{ required: true, message: 'Аты-жөн нөмерді енгізіңіз!' }]"
+        >
+          <a-input v-model:value="basicInfoForm.fullname" maxLength="30" />
+        </a-form-item>
 
-                <a-form-item label="Email" name="email"
-                    :rules="[{ required: true, message: 'Телефон нөмерді енгізіңіз!' }]">
-                    <a-input v-model:value="basicInfoForm.email" type="email" maxLength="254" />
-                </a-form-item>
+        <a-form-item
+          label="Email"
+          name="email"
+          :rules="[{ required: true, message: 'Телефон нөмерді енгізіңіз!' }]"
+        >
+          <a-input
+            v-model:value="basicInfoForm.email"
+            type="email"
+            maxLength="254"
+          />
+        </a-form-item>
 
-                <a-form-item>
-                    <a-button block type="primary" html-type="submit" :loading="updateInfoLoading">Сақтау</a-button>
-                </a-form-item>
-            </a-form>
-        </div>
-
-        <div class="block">
-            <p class="head">Құпиясөз өзгерту</p>
-
-            <a-form :model="changePasswordForm" layout="vertical" autocomplete="off" @finish="handleChangePassword">
-                <a-form-item label="Ескі құпиясөз" name="password"
-                    :rules="[{ required: true, message: 'Құпиясөзді енгізіңіз!' }]">
-                    <a-input-password v-model:value="changePasswordForm.password" maxLength="254" />
-                </a-form-item>
-
-                <a-form-item label="Жаңа құпиясөз" name="newPassword"
-                    :rules="[{ required: true, message: 'Құпиясөзді енгізіңіз!' }]">
-                    <a-input-password v-model:value="changePasswordForm.newPassword" maxLength="254" />
-                </a-form-item>
-
-                <a-form-item>
-                    <a-button block type="primary" html-type="submit" :loading="changePasswordLoading">Сақтау</a-button>
-                </a-form-item>
-            </a-form>
-        </div>
-
-        <a-button block danger type="primary" class="logout-btn" @click="handleLogout">
-            Шығу
-            <Icon name="octicon:sign-out" />
-        </a-button>
+        <a-form-item>
+          <a-button
+            block
+            type="primary"
+            html-type="submit"
+            :loading="updateInfoLoading"
+            >Сақтау</a-button
+          >
+        </a-form-item>
+      </a-form>
     </div>
+
+    <div class="block">
+      <p class="head">Құпиясөз өзгерту</p>
+
+      <a-form
+        :model="changePasswordForm"
+        layout="vertical"
+        autocomplete="off"
+        @finish="handleChangePassword"
+      >
+        <a-form-item
+          label="Ескі құпиясөз"
+          name="password"
+          :rules="[{ required: true, message: 'Құпиясөзді енгізіңіз!' }]"
+        >
+          <a-input-password
+            v-model:value="changePasswordForm.password"
+            maxLength="254"
+          />
+        </a-form-item>
+
+        <a-form-item
+          label="Жаңа құпиясөз"
+          name="newPassword"
+          :rules="[{ required: true, message: 'Құпиясөзді енгізіңіз!' }]"
+        >
+          <a-input-password
+            v-model:value="changePasswordForm.newPassword"
+            maxLength="254"
+          />
+        </a-form-item>
+
+        <a-form-item>
+          <a-button
+            block
+            type="primary"
+            html-type="submit"
+            :loading="changePasswordLoading"
+            >Сақтау</a-button
+          >
+        </a-form-item>
+      </a-form>
+    </div>
+
+    <a-button
+      block
+      danger
+      type="primary"
+      class="logout-btn"
+      @click="handleLogout"
+    >
+      Шығу
+      <Icon name="octicon:sign-out" />
+    </a-button>
+  </div>
 </template>
 
 <script setup lang="ts">
 // Vue
-import { ref } from 'vue';
+import { ref } from "vue";
 // Vue Router
-import { useRouter } from 'vue-router';
+import { useRouter } from "vue-router";
 // Store
-import { useUser } from '~/stores/user';
+import { useUserStore } from "~/stores/user";
 // API
-import { API_UpdateInfo, API_ChangePassword } from '~/service/api/user-api';
+import { API_UpdateInfo, API_ChangePassword } from "~/service/api/user-api";
 // Utils
-import { defaultUserState } from '~/stores/user';
+import { defaultUserState } from "~/stores/user";
 // Hooks
-import useRequest from 'vue-hooks-plus/es/useRequest';
+import useRequest from "vue-hooks-plus/es/useRequest";
 // Antd Component
-import { message as AntdMessage } from 'ant-design-vue';
+import { message as AntdMessage } from "ant-design-vue";
 
-const user = useUser();
+const userStore = useUserStore();
 const router = useRouter();
 
 const basicInfoForm = ref({
-    fullname: user.fullname,
-    email: user.email,
+  fullname: userStore.fullname,
+  email: userStore.email,
 });
 
 const changePasswordForm = ref({
-    password: '',
-    newPassword: '',
-})
+  password: "",
+  newPassword: "",
+});
 
-const { runAsync: updateInfo, loading: updateInfoLoading } = useRequest(API_UpdateInfo, { manual: true });
-const { runAsync: changePassword, loading: changePasswordLoading } = useRequest(API_ChangePassword, { manual: true });
+const { runAsync: updateInfo, loading: updateInfoLoading } = useRequest(
+  API_UpdateInfo,
+  { manual: true }
+);
+const { runAsync: changePassword, loading: changePasswordLoading } = useRequest(
+  API_ChangePassword,
+  { manual: true }
+);
 
 const handleChangeInfo = (values: any) => {
-    updateInfo(user.id, values).then(({ data }) => {
-        localStorage.set('user', { ...user.$state, ...data });
-        user.$state = { ...user.$state, ...data };
-        AntdMessage.success('Өзгеріс сақталды!');
-    }).catch(error => {
-        AntdMessage.error(error.message);
+  updateInfo(userStore.id, values)
+    .then(({ data }) => {
+      localStorage.set("user", { ...userStore.$state, ...data });
+      userStore.$state = { ...userStore.$state, ...data };
+      AntdMessage.success("Өзгеріс сақталды!");
     })
-}
+    .catch((error) => {
+      AntdMessage.error(error.message);
+    });
+};
 
 const handleChangePassword = (values: any) => {
-    changePassword(values).then(() => {
-        AntdMessage.success('Өзгеріс сақталды!');
-    }).catch(error => {
-        AntdMessage.error(error.message);
+  changePassword(values)
+    .then(() => {
+      AntdMessage.success("Өзгеріс сақталды!");
     })
-}
+    .catch((error) => {
+      AntdMessage.error(error.message);
+    });
+};
 
 const handleLogout = () => {
-    localStorage.set('user', defaultUserState);
-    user.$state = defaultUserState;
-    router.replace({ path: '/auth/login' });
-}
+  localStorage.set("user", defaultUserState);
+  userStore.$state = defaultUserState;
+  router.replace({ path: "/auth/login" });
+};
 </script>
 
 <style scoped lang="scss">
 @import "~/assets/style/mixins.scss";
 
 .profile-settings {
-    padding-bottom: 16px;
-    @include flex($gap: 16px, $wrap: wrap);
+  padding-bottom: 16px;
+  @include flex($gap: 16px, $wrap: wrap);
 
-    .block {
-        flex: 0 1 320px;
-        max-width: 100%;
-        padding: 8px 16px 16px;
-        /* margin-bottom: 24px; */
-        border-radius: var(--border-radius);
-        border: 1px solid var(--c-border);
+  .block {
+    flex: 0 1 320px;
+    max-width: 100%;
+    padding: 8px 16px 16px;
+    /* margin-bottom: 24px; */
+    border-radius: var(--border-radius);
+    border: 1px solid var(--c-border);
 
-        .head {
-            margin-bottom: 8px;
-        }
-
-        .ant-btn {
-            height: 44px;
-        }
-
-        .ant-form-item:last-child {
-            margin: 0;
-        }
+    .head {
+      margin-bottom: 8px;
     }
 
-    .logout-btn {
-        height: 44px;
-        @include flexCenter($gap: 8px);
-
-        svg {
-            @include svgStyle($color: #fff);
-        }
+    .ant-btn {
+      height: 44px;
     }
+
+    .ant-form-item:last-child {
+      margin: 0;
+    }
+  }
+
+  .logout-btn {
+    height: 44px;
+    @include flexCenter($gap: 8px);
+
+    svg {
+      @include svgStyle($color: #fff);
+    }
+  }
 }
 </style>
