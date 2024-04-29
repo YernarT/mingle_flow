3ad6c5a1808294f87ea1f2aaa37595557ddc8941
YernarT <toktaryernar@gmail.com>
2024-04-29 11:47:23 +0500

    update: :hammer: project api

--- frontend/src/service/api/project-api.ts
+++ frontend/src/service/api/project-api.ts
@@ -1,6 +1,6 @@
 // Types
 import type { I_Response } from "~/types/api";
-import type { I_Project } from "~/types/project";
+import type { I_Project, I_ProjectContributor } from "~/types/project";
 
 // Utils
 import _fetch from "~/service/fetch";
@@ -8,3 +8,18 @@ import _fetch from "~/service/fetch";
 export const API_FetchProjectList = (
   params = {}
 ): Promise<I_Response<I_Project[]>> => _fetch.get("/project/", params);
+
+export const API_FetchProject = (
+  projectId: number
+): Promise<I_Response<I_Project>> => _fetch.get(`/project/${projectId}/`);
+
+export const API_CreateProject = (
+  payload: object
+): Promise<I_Response<I_Project>> => {
+  // @ts-ignore
+  payload.contributorList = payload.contributorList.map(
+    (contributor: I_ProjectContributor) => contributor.id
+  );
+
+  return _fetch.post("/project/", payload);
+};
