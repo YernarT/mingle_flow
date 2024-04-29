59f1b190ea8e88bc55b285a5ad7290f5ea9ded06
YernarT <toktaryernar@gmail.com>
2024-04-29 11:41:20 +0500

    update: :hammer: to_representation

--- backend/apps/task/serializer.py
+++ backend/apps/task/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from user.serializer import UserSerializer
+from project.serializer import ProjectSerializer
 from task.models import Task, TaskAttachement, TaskComment
 
 
@@ -9,6 +10,21 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['creator'] = UserSerializer(instance=instance.creator, context=self.context).data
+        data['project'] = ProjectSerializer(instance=instance.project, context=self.context).data
+        data['tags'] = instance.tags.join(',')
+        if instance.worker:
+            data['worker'] = UserSerializer(instance=instance.worker, context=self.context).data
+        attachement_list = TaskAttachement.objects.filter(task=instance)
+        data['attachement_list'] = TaskAttachementSerializer(
+            instance=attachement_list, many=True).data
+        comment_list = TaskComment.objects.filter(task=instance)
+        data['comment_list'] = TaskCommentSerializer(instance=comment_list, many=True).data
+
+        return data
 
 
 class TaskAttachementSerializer(serializers.HyperlinkedModelSerializer):
