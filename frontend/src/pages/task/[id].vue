438cb8eaad86537f1b85a66171d41d42e302b58e
YernarT <toktaryernar@gmail.com>
2024-04-29 11:46:43 +0500

    wip: :contruction: task detail page

---
+++ frontend/src/pages/task/[id].vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="task-detail-page">
+    Task detail page
+    {{ $route.params.id }}
+  </main>
+</template>
+
+<script setup>
+defineOptions({ name: "TaskDetailPage" });
+</script>
+
+<style scoped lang="scss">
+@import "~/assets/style/mixins.scss";
+
+.task-detail-page {
+}
+</style>
