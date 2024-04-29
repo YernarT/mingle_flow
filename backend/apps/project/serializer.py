0f80f06d1d1171a7ceb5c1c77bce97c5afefd165
YernarT <toktaryernar@gmail.com>
2024-04-29 11:40:25 +0500

    feat: :sparkles: create project

--- backend/apps/project/serializer.py
+++ backend/apps/project/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from user.serializer import UserSerializer
+from user.models import User
+
 from project.models import Project, Contributor, ProjectStatuses
 from task.models import Task
 
@@ -18,11 +20,22 @@ class ProjectSerializer(serializers.ModelSerializer):
         contributor_count = contributor_list.count()
         data['contributor_count'] = contributor_count
         data['contributor_list'] = ContributorSerializer(
-            instance=contributor_list[:7], many=True).data
+            instance=contributor_list[:7], many=True, context=self.context).data
         data['task_count'] = Task.objects.filter(project=instance).count()
         data['progress'] = instance.get_progress()
+        data['creator'] = UserSerializer(instance=instance.creator, context=self.context).data
 
         return data
+
+    def create(self, validated_data):
+        project = super().create(validated_data)
+        contributor_list = self.context.get('request').data.get('contributor_list')
+
+        for contributor in contributor_list:
+            contributor = User.objects.get(id=contributor)
+            Contributor.objects.create(project=project, user=contributor)
+
+        return project
 
 
 class ContributorSerializer(serializers.ModelSerializer):
