1fd3cd9948abd1ee0d523e3c7e28ff6f03ed4e39
YernarT <toktaryernar@gmail.com>
2024-05-10 22:00:28 +0500

    update: :hammer: task 常量

--- frontend/src/constants/task.ts
+++ frontend/src/constants/task.ts
@@ -1,4 +1,4 @@
-import type { T_Filter } from "types/task";
+import type { T_Filter, T_TaskPriority, T_TaskStatus } from "types/task";
 
 export const TASK = {
   filter: [
@@ -15,4 +15,51 @@ export const TASK = {
       value: "assigned",
     },
   ] as { label: string; value: T_Filter }[],
+
+  priority: {
+    none: 0 as T_TaskPriority,
+    low: 1 as T_TaskPriority,
+    medium: 2 as T_TaskPriority,
+    high: 3 as T_TaskPriority,
+    urgent: 4 as T_TaskPriority,
+  },
+
+  priorityOptions: [
+    { label: "Жоқ", value: 0 },
+    { label: "Төмен", value: 1 },
+    { label: "Орташа", value: 2 },
+    { label: "Жоғары", value: 3 },
+    { label: "Шұғыл", value: 4 },
+  ],
+
+  status: {
+    backlog: 0 as T_TaskStatus,
+    analyze: 1 as T_TaskStatus,
+    develop: 2 as T_TaskStatus,
+    test: 3 as T_TaskStatus,
+    finish: 4 as T_TaskStatus,
+  },
+
+  statusOptions: [
+    {
+      label: "Архив",
+      value: 0,
+    },
+    {
+      label: "Талдау",
+      value: 1,
+    },
+    {
+      label: "Әзірлеу",
+      value: 2,
+    },
+    {
+      label: "Сынақ",
+      value: 3,
+    },
+    {
+      label: "Аяқталған",
+      value: 4,
+    },
+  ],
 };
