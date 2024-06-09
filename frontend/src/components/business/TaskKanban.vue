a2a632bfe278da8098fd70cee4b976acf4c13d09
YernarT <toktaryernar@gmail.com>
2024-06-09 20:05:25 +0500

    feat: :sparkles: draggable

--- frontend/src/components/business/TaskKanban.vue
+++ frontend/src/components/business/TaskKanban.vue
@@ -1,46 +1,19 @@
 <template>
-  <div class="task-kanban">
-    <div class="list-wrap">
-      <div class="head">Архив ({{ taskList.backlog.length }})</div>
-      <div class="list">
-        <TaskCard
-          v-for="task in taskList.backlog"
-          :key="task.id"
-          :task="task"
-        />
-      </div>
-    </div>
-    <div class="list-wrap">
-      <div class="head">Талдау ({{ taskList.analyze.length }})</div>
-      <div class="list">
-        <TaskCard
-          v-for="task in taskList.analyze"
-          :key="task.id"
-          :task="task"
-        />
-      </div>
-    </div>
-    <div class="list-wrap">
-      <div class="head">Әзірлеу ({{ taskList.develop.length }})</div>
-      <div class="list">
-        <TaskCard
-          v-for="task in taskList.develop"
-          :key="task.id"
-          :task="task"
-        />
-      </div>
-    </div>
-    <div class="list-wrap">
-      <div class="head">Сынақ ({{ taskList.test.length }})</div>
-      <div class="list">
-        <TaskCard v-for="task in taskList.test" :key="task.id" :task="task" />
-      </div>
-    </div>
-    <div class="list-wrap">
-      <div class="head">Аяқталған ({{ taskList.finish.length }})</div>
-      <div class="list">
-        <TaskCard v-for="task in taskList.finish" :key="task.id" :task="task" />
-      </div>
+  <div class="itisit-container task-kanban">
+    <div v-for="column in columns" :key="column.value" class="column">
+      <p class="column__head">
+        {{ column.label }} {{ column.taskList.length }}
+      </p>
+      <VueDraggableNext
+        :list="column.taskList"
+        :animation="200"
+        class="column__list"
+        :data-state="column.value"
+        group="taskList"
+        @end="handleMoveCard"
+      >
+        <TaskCard v-for="task in column.taskList" :key="task.id" :task="task" />
+      </VueDraggableNext>
     </div>
   </div>
 </template>
@@ -49,8 +22,15 @@
 // Types
 import type { I_Task } from "@/types/task";
 
+// Vue
+import { computed } from "vue";
 // Components
 import TaskCard from "@/components/business/TaskCard/index.vue";
+import { VueDraggableNext } from "vue-draggable-next";
+// Constants
+import { TASK } from "@/constants/task";
+// Utils
+import _ from "lodash";
 
 defineOptions({ name: "TaskKanban" });
 
@@ -63,26 +43,59 @@ const props = defineProps<{
     finish: I_Task[];
   };
 }>();
+
+const emit = defineEmits(["changeState"]);
+
+const columns = computed(() => {
+  return TASK.statusOptions.map((status) => {
+    const statusKey = _.findKey(
+      TASK.status,
+      (statusValue) => statusValue === status.value
+    );
+
+    // @ts-ignore
+    return { ...status, taskList: props.taskList[statusKey] };
+  });
+});
+
+const handleMoveCard = (event: any) => {
+  const fromState = event.from.getAttribute("data-state");
+  const toState = event.to.getAttribute("data-state");
+  if (fromState === toState) return;
+
+  const statusKey = _.findKey(
+    TASK.status,
+    (statusValue) => statusValue === Number(toState)
+  );
+
+  // @ts-ignore
+  const task = props.taskList[statusKey][event.newIndex];
+  emit("changeState", task, Number(toState));
+};
 </script>
 
 <style scoped lang="scss">
 @import "~/assets/style/mixins.scss";
 
 .task-kanban {
+  width: 100%;
+  margin-bottom: 24px;
+  flex: 1 1 100%;
   @include flex($gap: 8px);
 
-  .list-wrap {
-    flex: 1 1 100%;
-    @include flex($direction: column, $gap: 8px);
+  .column {
+    flex: 1 1 calc((100% - 8px * 4) / 5);
 
-    .head {
+    &__head {
       font-size: 18px;
       margin-bottom: 8px;
     }
 
-    .list {
+    &__list {
       width: 100%;
-      @include flex($direction: column, $gap: 8px);
+      max-height: 100dvh;
+      overflow: hidden auto;
+      @include flex($direction: column, $gap: 16px);
     }
   }
 }
